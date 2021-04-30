ea91587136d282c518962c08d28bdd567144dc49
Kelvin <kt16@sanger.ac.uk>
2021-04-30 17:00:17 +0100

    rearrange folders

---
+++ BIN/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# @Author: Kelvin
+# @Date:   2021-04-30 16:56:14
+# @Last Modified by:   Kelvin
+# @Last Modified time: 2021-04-30 16:59:03
+
+from . import _functions as fn
+from fn import *
+
+__all__ = [
+    'fn',
+]
