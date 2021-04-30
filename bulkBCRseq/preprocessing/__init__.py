046e45e781893e30fa8f3f5f2bf1d9962b914b22
Kelvin <kt16@sanger.ac.uk>
2021-04-30 17:09:35 +0100

    make a copy of files

---
+++ bulkBCRseq/preprocessing/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# @Author: Kelvin
+# @Date:   2021-04-30 16:56:14
+# @Last Modified by:   Kelvin
+# @Last Modified time: 2021-04-30 17:00:59
+
+from . import functions as fn
+from fn import *
+
+__all__ = [
+    'fn',
+]
