389619b23d0cf8fdc84b1184137e08307f5e2565
Kelvin <kt16@sanger.ac.uk>
2021-04-30 16:53:56 +0100

    Create __init__.py

---
+++ BIN/_functions/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# @Author: Kelvin
+# @Date:   2021-04-30 15:35:31
+# @Last Modified by:   Kelvin
+# @Last Modified time: 2021-04-30 16:25:48
+
+from _functions import Tree, fuzzy_substring, do_align, deconvolute_same_array, get_codons, init_rc, reverse_comp
+
+__all__ = [
+    'Tree',
+    'fuzzy_substring',
+    'do_align',
+    'deconvolute_same_array',
+    'get_codons',
+    'init_rc',
+    'reverse_comp',
+]
