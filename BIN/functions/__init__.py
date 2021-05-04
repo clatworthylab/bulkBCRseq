3928939bd5de9c1dd280a998b563b3c36c28ff21
Kelvin <kt16@sanger.ac.uk>
2021-05-04 16:01:58 +0100

    Update __init__.py

--- BIN/functions/__init__.py
+++ BIN/functions/__init__.py
@@ -2,11 +2,11 @@
 # @Author: Kelvin
 # @Date:   2021-04-30 15:35:31
 # @Last Modified by:   Kelvin
-# @Last Modified time: 2021-05-04 15:12:33
+# @Last Modified time: 2021-05-04 16:01:19
 
 from ._functions import (Tree, fuzzy_substring, do_align,
                          deconvolute_same_array, get_codons, init_rc,
-                         reverse_comp, fasta_iterator)
+                         reverse_comp, fasta_iterator, translate)
 
 __all__ = [
     'Tree',
@@ -17,4 +17,5 @@ __all__ = [
     'init_rc',
     'reverse_comp',
     'fasta_iterator',
+    'translate',
 ]
