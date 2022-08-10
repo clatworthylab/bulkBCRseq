13eb61eba348922cb10ad65495b42d6f3f79d759
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-08-10 20:31:46 +0100

    ok

--- isotyper/utilities/__init__.py
+++ isotyper/utilities/__init__.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 from isotyper.utilities._utils import (
     deconvolute_same_array,
+    EDGE_LENGTHS,
+    EXTPATH,
     fasta_iterator,
     fuzzy_substring,
     get_codons,
     init_rc,
     LIBPATH,
+    PERLCMD,
+    READ_NUMBER_DIVISION
     reverse_comp,
     translate,
     Tree,
@@ -14,11 +18,15 @@ from isotyper.utilities._utils import (
 
 __all__ = [
     "deconvolute_same_array",
+    "EDGE_LENGTHS",
+    "EXTPATH",
     "fasta_iterator",
     "fuzzy_substring",
     "get_codons",
     "init_rc",
     "LIBPATH",
+    "PERLCMD",
+    "READ_NUMBER_DIVISIO"N
     "reverse_comp",
     "translate",
     "Tree",
