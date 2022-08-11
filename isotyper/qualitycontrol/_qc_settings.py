6a9203ca27c81f79878b2a5ee987c162cfdf0bff
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-08-11 10:32:25 +0100

    Update _qc_settings.py

--- isotyper/qualitycontrol/_qc_settings.py
+++ isotyper/qualitycontrol/_qc_settings.py
@@ -8,12 +8,15 @@ from isotyper.utilities._args import (
     OUT_ORTSEQ_TMP,
 )
 
-
 # Files for Step2
 PRIMER_FILE = LIBPATH / f"Primers_{ORG}_IG_RBR_Constant_region_MPLX_table.txt"
-TMP_FILE = OUT_NET / f"NN_Tmp_cluster_{SAMPLE_ID}.txt"
-FAIL_FILE = OUT_FASTQ / f"Fail_filtered_{SAMPLE_ID}.fasta"
+
+# get overlapping paired reads
 SEQ_FASTA_FILE1 = OUT_FASTQ / f"Sequences_{SAMPLE_ID}_1.fasta"
 SEQ_FASTA_FILE2 = OUT_FASTQ / f"Sequences_{SAMPLE_ID}_2.fasta"
 UNTRIMMED_FASTA = OUT_ORTSEQ_TMP / f"Untrimmed_{SAMPLE_ID}.fasta"
+
+# trimming
+TMP_FILE = OUT_NET / f"NN_Tmp_cluster_{SAMPLE_ID}.txt"
+FAIL_FILE = OUT_FASTQ / f"Fail_filtered_{SAMPLE_ID}.fasta"
 PRIMER_TAG_FILE = OUT_ORTSEQ_TMP / f"All_barcodes_{SAMPLE_ID}.txt"
