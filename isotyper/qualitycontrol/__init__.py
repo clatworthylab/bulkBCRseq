6771314d8e4780e9c31c458b02a842da51299ac5
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-08-11 10:08:00 +0100

    Update __init__.py

--- isotyper/qualitycontrol/__init__.py
+++ isotyper/qualitycontrol/__init__.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
-
-#!/usr/bin/env python
 from isotyper.qualitycontrol._qc_settings import (
     FAIL_FILE,
     PRIMER_FILE,
+    SEQ_FASTA_FILE1,
+    SEQ_FASTA_FILE2,
     TMP_FILE,
+    UNTRIMMED_FASTA,
 )
 
 __all__ = [
     "FAIL_FILE",
     "PRIMER_FILE",
+    "SEQ_FASTA_FILE1",
+    "SEQ_FASTA_FILE2",
     "TMP_FILE",
+    "UNTRIMMED_FASTA",
 ]
