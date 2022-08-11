de978d005c734b0a9a169c99c4b91ea41779fa8f
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-08-11 12:04:34 +0100

    Create test_check2.py

---
+++ tests/test_check2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from subprocess import run
+
+import pytest
+
+
+@pytest.mark.parametrize("option", [1, 2, 3, 4])
+def test_call_script(option):
+    cmd = [
+        "python",
+        "Processing_sequences_large_scale.py",
+        "tests/data/Sample_metadata.txt",
+        str(option),
+        "N",
+        "Y",
+        "Y",
+    ]
+    run(cmd)
