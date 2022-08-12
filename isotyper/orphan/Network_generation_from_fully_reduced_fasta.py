891f64d1eae60273f58908081ceee0e602050c13
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-08-12 11:22:18 +0100

    Update Network_generation_from_fully_reduced_fasta.py

--- isotyper/orphan/Network_generation_from_fully_reduced_fasta.py
+++ isotyper/orphan/Network_generation_from_fully_reduced_fasta.py
@@ -29,6 +29,44 @@ import networkx as nx
 import re
 
 
+def fuzzy_substring(needle, haystack):
+    """
+    Calculate the fuzzy match of needle in haystack, using a modified version of the Levenshtein distance algorithm.
+    The function is modified from the levenshtein function in the bktree module by Adam Hupp
+    Parameters
+    ----------
+    needle : TYPE
+        Description
+    haystack : TYPE
+        Description
+    Returns
+    -------
+    TYPE
+        Description
+    """
+    m, n = len(needle), len(haystack)
+    # base cases
+    if m == 1:
+        return needle not in haystack
+    if not n:
+        return m
+    row1 = [0] * (n + 1)
+    for i in range(0, m):
+        row2 = [i + 1]
+        for j in range(0, n):
+            cost = needle[i] != haystack[j]
+
+            row2.append(
+                min(
+                    row1[j + 1] + 1,  # deletion
+                    row2[j] + 1,  # insertion
+                    row1[j] + cost,
+                )  # substitution
+            )
+        row1 = row2
+    return min(row1)
+
+
 def fasta_iterator(fh1):
     """Summary
 
